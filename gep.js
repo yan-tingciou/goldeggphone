9b9db96f4a850fbbd4e8c40f60999680f2764edd
yan-tingciou <angelaavril04@gmail.com>
2023-04-27 21:23:04 +0800

    新增環境10

--- gep.js
+++ gep.js
@@ -1,7 +1,30 @@
-$(document).ready(function () {
-    $('.menu > li > a').click(function(event){
-        event.preventDefault();
-        $(this).parent().find('ul').slideToggle();
-        $(this).parent().siblings().find('ul').slideUp();
-      });
+$(document).ready(function(){
+  $('.menu > li > a').click(function(event){
+    event.preventDefault();
+    $(this).parent().find('ul').slideToggle();
+    $(this).parent().siblings().find('ul').slideUp();
+  });
+  const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  autoplay: {
+  delay: 5000,
+  },
+  effect: 'slide',
+  slideShadows: 'false',
+  direction: 'horizontal',
+  speed: 800,
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
+});
+
